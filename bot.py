0f425a74d9aac4497e59da65995e180134d5eb1b
admin <m.a.savin@bk.ru>
2026-02-02 17:59:10 +0300

    update env

--- bot.py
+++ bot.py
@@ -23,17 +23,13 @@ dp = Dispatcher()
 PROMPT = (
     "You are the editor of the satirical and ironic inform. agencies. Your task is to write humorous news in a satirical and ironic way, which you can laugh at for the telegram channel. The news can be both from Russia and from all over the world."
 
-    "Emphasize the absurdity and subtle humor"
-    
-    "One news item is one message."
+    "Emphasize the absurdity and subtle humor. Write in the following format.\n\n *Only one news item in one message*\n"
 
-    "Write in the following format"
+    "⚡️ Post headline\n"
 
-    "⚡️ News headline"
+    "The text of the news item. \n 3-6 sentences without explanations\n"
 
-    "The text of the news.3-6 sentences without explanations"
-
-    "Important: when translating a text into Russian, review the text again because sometimes the meaning is very difficult to understand"
+    "*Important: when translating a text into Russian, review the text again because sometimes the meaning is very difficult to understand*"
 )
 
 # Для "цепочки сообщений", можно хранить parent_message_id
@@ -92,7 +88,7 @@ async def main():
     Основная функция
     """
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(send_news, "interval", minutes=1)
+    scheduler.add_job(send_news, "interval", minutes=30)
     scheduler.start()
 
     print("[INFO] Бот запущен. Рассылка новостей каждые 30 минут с 9:00 до 23:00.")
@@ -101,20 +97,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# import requests
-#
-# url = "https://api.timeweb.cloud/api/v1/cloud-ai/agents/986ff584-52c1-4135-a6b5-1764c3a4b021/call"
-#
-# payload = {
-#     "message": "Привет",
-#     "parent_message_id": "3adfea84-bcdb-44b5-8914-92035e75ec24"
-# }
-# headers = {
-#     "content-type": "application/json",
-#     "authorization": "Bearer eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjFrYnhacFJNQGJSI0tSbE1xS1lqIn0.eyJ1c2VyIjoiaXM0MDA3MyIsInR5cGUiOiJhcGlfa2V5IiwiYXBpX2tleV9pZCI6IjQyOTM2NGEyLTAyMjktNGQxMy1hNTM3LTZiMjJmYTE0M2M0NSIsImlhdCI6MTc3MDAyNzk2M30.TTUi0ND9mxalnZ44ZpPykZnbn26jWt0bMGxK38fQAUGlS1dVaWjHJ3fOAIq8elVuxhJO8Lpru_JCZvq2ym6MtN4oMLH0rJFgLr_n9SHOx78sIUDVAq5xOkDJ9gxHdSOixXiAhw44A0dQADJW6vvZH71SHHtH1ROhXsadBZV-WBNJLKsV0ZMB84DO7tvM09K8XkukhWa1Sqgsm5l_cDarqn4K6fz8KdwMjlIu1TgTpiQ7bSzpIydHOuPC_N3aYqlGugRFPraEQZ4TvS75S8_cFB0g1DOcg0ykKQAGvY0Oyl8kaIkGr8pSZQj_0ycpAGsOE3lKV4_wEGMFJTRK3C2f6jxiDFjoDnjqoszsuObSwUCX9slTqC_dg2N-045lXBSzsIUTPzJjrFiYgPexRnGyg2Ni75t7l3kBMyoWdj6dD80LAF9NJyd2yg9Jlr2bZmghhwvMtlZ7n1HYBxYHBKuYF53VbtWaUpJXv_OPveSZ2KfR1wO--fALjHvjcItThHRR"
-# }
-#
-# response = requests.post(url, json=payload, headers=headers)
-#
-# print(response.json())
